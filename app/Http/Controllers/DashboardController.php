4925c940f46d6a0565180148c4c4f8a142103dda
Abcinsy <ysabianca.delarama@gmail.com>
2024-06-26 12:44:12 +0800

    creating models

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,32 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Idea;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        $users = [
-            [
-                'name'=> 'Alex',
-                'age' => 30,
-            ],
-            [
-                'name'=> "Dan",
-                'age' => 25,
-            ],
-            [
-                'name'=> "John",
-                'age' => 17,
-            ]
-        ];
+        $idea = new Idea([
+            'content' => 'Hello youtube',
+        ]);
+        $idea->save();
 
-        return view(
-            'dashboard',
-            [
-                'users'=> $users
-            ]
-        );
+        // dump(Idea::all());
+
+        return view('dashboard',[
+            'ideas' => Idea::orderBy('created_at', 'desc')->get()
+        ]);
     }
 }
