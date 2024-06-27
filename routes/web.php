9b3413f34f45312e3c61bfe1ede429c90e995d92
Abcinsy <ysabianca.delarama@gmail.com>
2024-06-27 18:57:00 +0800

    added comment functionality

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\IdeaController;
+use App\Http\Controllers\CommentController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -13,6 +14,9 @@ Route::get('/ideas/{idea}', [IdeaController::class, 'show'])->name('ideas.show')
 Route::get('/ideas/{idea}/edit', [IdeaController::class, 'edit'])->name('ideas.edit');
 Route::put('/ideas/{idea}', [IdeaController::class, 'update'])->name('ideas.update');
 Route::delete('/ideas/${idea}', [IdeaController::class, 'destroy'])->name('ideas.destroy');
+
+// Comments
+Route::post('/ideas/{idea}/comments', [CommentController::class, 'store'])->name('ideas.comments.store');
 
 Route::get('/terms', function () {
     return view('terms');
